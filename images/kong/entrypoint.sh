8ab7090224b3bab06bb370060fe90c9defd5147a
Webysther <webysther@gmail.com>
2026-02-13 20:00:10 +0100

    ✨ feat(kong): inject environment variables into kong.yml at startup

--- images/kong/entrypoint.sh
+++ images/kong/entrypoint.sh
@@ -3,3 +3,9 @@ set -Eeuo pipefail
 
 export SUPABASE_ANON_KEY="${SUPABASE_ANON_KEY:-$ANON_KEY}"
 export SUPABASE_SERVICE_KEY="${SUPABASE_SERVICE_KEY:-$SERVICE_ROLE_KEY}"
+
+sed -i "s|\$SUPABASE_ANON_KEY|${SUPABASE_ANON_KEY}|g" /home/kong/kong.yml
+sed -i "s|\$SUPABASE_SERVICE_KEY|${SUPABASE_SERVICE_KEY}|g" /home/kong/kong.yml
+
+sed -i "s|\$DASHBOARD_USERNAME|${DASHBOARD_USERNAME}|g" /home/kong/kong.yml
+sed -i "s|\$DASHBOARD_PASSWORD|${DASHBOARD_PASSWORD}|g" /home/kong/kong.yml
